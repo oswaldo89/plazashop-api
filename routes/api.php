b6753f09441749296506b74a01560cf8517dcdf3
Oswaldo Gomez <oswaldo.gh89@gmail.com>
2018-01-05 22:26:47 -0600

    se abre una url al publico para ver los productos siempre

--- routes/api.php
+++ routes/api.php
@@ -9,5 +9,6 @@ Route::post('login', 'Api\AuthController@login');
 Route::group(['middleware' => 'auth:api'], function () {
     Route::post('profile', 'Api\AuthController@profile');
     Route::resource('product', 'Api\ProductController');
-    Route::get('/product_list/{total}', 'Api\ProductController@getList');
 });
+//url abierta al publico
+Route::get('/product_list/{total}', 'Api\ProductController@getList');
