5f1cd32248b44ab13c728112a49ff09fce495cb2
Oswaldo Gomez <oswaldo.gh89@gmail.com>
2018-02-10 22:10:44 -0600

    comenta

--- database/migrations/2018_02_11_003509_create_products_photos_table.php
+++ database/migrations/2018_02_11_003509_create_products_photos_table.php
@@ -6,11 +6,6 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateProductsPhotosTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('products_photos', function (Blueprint $table) {
@@ -21,12 +16,7 @@ class CreateProductsPhotosTable extends Migration
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+    
     public function down()
     {
         Schema::drop('products_photos');
