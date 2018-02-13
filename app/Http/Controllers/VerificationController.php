246ca99c5b32529c19c699e71d774048b3105dae
Oswaldo Gomez <oswaldo.gh89@gmail.com>
2018-02-12 23:00:11 -0600

    redirect succes

--- app/Http/Controllers/VerificationController.php
+++ app/Http/Controllers/VerificationController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class VerificationController extends Controller
 {
-    //
+    public function show($confirmation_code)
+    {
+        $response['status'] = null;
+        $response['message'] = null;
+        $response['user'] = null;
+        $user = User::where("confirmation_code",$confirmation_code)->first();
+        if (!$user)
+        {
+            $response['message'] = "Lo sentimos pero no se pudo validar su cuenta.";
+            return view('pages.confirmation_code', compact('response'));
+        }else{
+            $user->confirmed = 1;
+            $user->confirmation_code = null;
+            $user->save();
+            $response['status'] = true;
+            $response['user'] = $user->ceoName;
+            $response['message'] = "Su cuenta se ha validado correctamente, ahora puede iniciar sesión.";
+            return view('pages.confirmation_code', compact('response'));
+        }
+    }
 }
