f466ba141a0f1e49cc60fe7abe7e924069ef54c2
Oswaldo Gomez <oswaldo.gh89@gmail.com>
2018-04-16 23:46:14 -0500

    se agrega eliminar imagen

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::resource('product', 'Api\ProductController');
     Route::get('/product_listByUser/{total}/{user_id}', 'Api\ProductController@getListByUser');
     Route::post('/updatePet', 'Api\ProductController@updateProduct');
-    Route::post('/deleteImage', 'Api\ProductController@deleteImage');
+    Route::post('/deleteImage/{id}', 'Api\ProductController@deleteImage');
 });
 //url abierta al publico
 Route::get('/product_list/{total}', 'Api\ProductController@getList');
