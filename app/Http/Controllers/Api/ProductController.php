523f9f06b155df2a3e7c60147b3016bee05127b7
Oswaldo Gomez <oswaldogh89@MacBook-Pro-de-Oswaldo.local>
2018-04-22 03:24:28 -0500

    curl

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -136,7 +136,7 @@ class ProductController extends Controller
     {
         $user_id = Auth::user()->id;
         $buyer_id = $request->buyer_id; //78
-        $message = $request->$message; //78
+        $message = $request->message; //78
         $product = Product::where("id", $request->pet_id)->first();
 
         //si el dueño del producto es el que esta en session envia la burbuja a la derecha
