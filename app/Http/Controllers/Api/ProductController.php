a54369eb26f75cc4fd68982f2b9b8d794be27d28
Oswaldo Gomez <oswaldo.gh89@gmail.com>
2018-04-29 19:32:00 -0500

    data

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -249,12 +249,14 @@ class ProductController extends Controller
      */
     private function sendNotification($post_data)
     {
-        return Curl::to('https://fcm.googleapis.com/fcm/send')
+        $response = Curl::to('https://fcm.googleapis.com/fcm/send')
             ->withData($post_data)
             ->asJson()
             ->withHeader('Content-Type: application/json')
             ->withHeader('Authorization: Key=AAAAEZ0_Zqc:APA91bFYBorBc7GJdzyj-Cp_3tY_UV4gklGUEJtnf0zp6J9KFDupcTohK81CzqOK6SfRpKVBp9ctpZx8Da0ibuBkJrfO7MKHcQzRLSdkzoy88TyVfnmVHc6z41AQ1jMFuMBYgURoMBrb')
             ->post();
+        Log::info('Log message', $post_data);
+        return $response;
     }
 
     private function subscribeUser($user_token, $conversation_uuid)
