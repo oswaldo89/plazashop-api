53d099e62c73d4e08f1a2b275e31fdff358b8968
Oswaldo Gomez <oswaldo.gh89@gmail.com>
2018-06-10 19:48:47 -0500

    se agrega libreria de slack

--- config/app.php
+++ config/app.php
@@ -178,6 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         Laravel\Passport\PassportServiceProvider::class,
         Ixudra\Curl\CurlServiceProvider::class,
+        jeremykenedy\Slack\Laravel\ServiceProvider::class,
 
     ],
 
@@ -228,6 +229,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Curl'          => Ixudra\Curl\Facades\Curl::class,
+        'Slack' => jeremykenedy\Slack\Laravel\Facade::class,
 
     ],
 
---
+++ config/slack.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'endpoint' => 'https://hooks.slack.com/services/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',
+    'channel' => '#general',
+    'username' => 'Puppy Love',
+    'icon' => ':ghost:',
+    'link_names' => false,
+    'unfurl_links' => false,
+    'unfurl_media' => true,
+    'allow_markdown' => true,
+    'markdown_in_attachments' => ['pretext', 'text', 'title', 'fields', 'fallback'],
+];
